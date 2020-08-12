bf67f28c2437b1e1281659e2b73c857e984b241a
artyom <kotifnat@gmail.com>
2020-08-12 16:22:35 +0300

    bug fixes

--- main.py
+++ main.py
@@ -1,44 +1,43 @@
-import datetime
-import requests
 import re
-from tzlocal import get_localzone
 
-# url = 'https://api.telegram.org/bot1372295846:AAEka6_LQCCEMMCYJ8SQyXXgJGW65xelC-I/'
+import requests
+import datetime
 
 
-class MyBot:
+class BotHandler:
 
     def __init__(self, token):
         self.token = token
-        self.api_url = f'https://api.telegram.org/bot{token}/'
+        self.api_url = "https://api.telegram.org/bot{}/".format(token)
 
-    def get_updates(self, timeout=45, offset=None):
+    def get_updates(self, offset=None, timeout=None):
         method = 'getUpdates'
         params = {'timeout': timeout, 'offset': offset}
-        response = requests.get(self.api_url + method, data=params).json()
-        return response['result']
+        resp = requests.get(self.api_url + method, params)
+        result_json = resp.json()['result']
+        return result_json
 
     def send_message(self, chat_id, text):
         params = {'chat_id': chat_id, 'text': text}
         method = 'sendMessage'
-        response = requests.post(self.api_url + method, data=params)
-        return response
+        resp = requests.post(self.api_url + method, params)
+        return resp
 
-    def get_last_update(self):
-        result = self.get_updates()
+    def get_last_update(self, offset):
+        get_result = self.get_updates(offset)
 
-        if len(result) > 0:
-            last_update = result[-1]
+        if len(get_result) > 0:
+            last_update = get_result[-1]
         else:
-            last_update = result[len(result)]
+            last_update = None
+
         return last_update
 
 
-token_api = '1372295846:AAEka6_LQCCEMMCYJ8SQyXXgJGW65xelC-I'
-my_bot = MyBot(token=token_api)
-greetings = ("здравствуй", "ку", "привет")
-tz = get_localzone()
-now = datetime.datetime.now(tz)
+token = '1372295846:AAEka6_LQCCEMMCYJ8SQyXXgJGW65xelC-I'
+my_bot = BotHandler(token)
+greetings = ('здравствуй', 'привет', 'ку', 'здорово')
+now = datetime.datetime.now()
 operators = {
     '+': lambda x, y: x + y,
     '-': lambda x, y: x - y,
@@ -49,43 +48,42 @@ operators = {
 
 def main():
     new_offset = None
-    today = now.day
     hour = now.hour
 
     while True:
-        my_bot.get_updates(offset=new_offset)
 
-        last_update = my_bot.get_last_update()
+        last_update = my_bot.get_last_update(new_offset)
 
-        last_update_id = last_update['update_id']
-        last_chat_text = last_update['message']['text']
-        last_chat_id = last_update['message']['chat']['id']
-        last_chat_name = last_update['message']['chat']['first_name']
+        if last_update:
+            last_update_id = last_update['update_id']
+            last_chat_text = last_update['message']['text']
+            last_chat_id = last_update['message']['chat']['id']
+            last_chat_name = last_update['message']['chat']['first_name']
 
-        if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
-            my_bot.send_message(last_chat_id, f'Доброе утро, {last_chat_name}')
+            if last_chat_text.lower() in greetings and 6 <= hour < 12:
+                my_bot.send_message(last_chat_id, 'Доброе утро, {}'.format(last_chat_name))
+
+            elif last_chat_text.lower() in greetings and 12 <= hour < 17:
+                my_bot.send_message(last_chat_id, 'Добрый день, {}'.format(last_chat_name))
+
+            elif last_chat_text.lower() in greetings and 17 <= hour < 23:
+                my_bot.send_message(last_chat_id, 'Добрый вечер, {}'.format(last_chat_name))
+            else:
+                try:
+                    operand_1, operator, operand_2 = re.split(r'([+\-*/])', last_chat_text)
+                    operand_1 = int(operand_1)
+                    operand_2 = int(operand_2)
+                    if operator in operators:
+                        result = operators[operator](operand_1, operand_2)
+                        my_bot.send_message(last_chat_id, f'{last_chat_text} = {result}')
+                except ValueError:
+                    my_bot.send_message(last_chat_id, f'Я Вас не понимаю, {last_chat_name}')
+
             new_offset = last_update_id + 1
-
-        elif last_chat_text.lower() in greetings and today == now.day and 12 <= hour < 17:
-            my_bot.send_message(last_chat_id, f'Добрый день, {last_chat_name}')
-            new_offset = last_update_id + 1
-
-        elif last_chat_text.lower() in greetings and today == now.day and 17 <= hour < 23:
-            my_bot.send_message(last_chat_id, f'Добрый вечер, {last_chat_name}')
-            new_offset = last_update_id + 1
-        else:
-            try:
-                operand_1, operator, operand_2 = re.split(r'([+\-*/])', last_chat_text)
-                operand_1 = int(operand_1)
-                operand_2 = int(operand_2)
-                if operator in operators:
-                    result = operators[operator](operand_1, operand_2)
-                    my_bot.send_message(last_chat_id, f'{last_chat_text} = {result}')
-                    new_offset = last_update_id + 1
-            except ValueError:
-                my_bot.send_message(last_chat_id, f'Я Вас не понимаю, {last_chat_name}')
-                new_offset = last_update_id + 1
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        exit()

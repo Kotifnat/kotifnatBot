2c32ddd6a7257524191d5b130f83eb65c15fddba
artyom <kotifnat@gmail.com>
2020-08-12 14:16:48 +0300

    add regular expression in calculator

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import datetime
 import requests
+import re
 from tzlocal import get_localzone
 
-url = 'https://api.telegram.org/bot1372295846:AAEka6_LQCCEMMCYJ8SQyXXgJGW65xelC-I/'
+# url = 'https://api.telegram.org/bot1372295846:AAEka6_LQCCEMMCYJ8SQyXXgJGW65xelC-I/'
 
 
 class MyBot:
@@ -38,6 +39,12 @@ my_bot = MyBot(token=token_api)
 greetings = ("здравствуй", "ку", "привет")
 tz = get_localzone()
 now = datetime.datetime.now(tz)
+operators = {
+    '+': lambda x, y: x + y,
+    '-': lambda x, y: x - y,
+    '*': lambda x, y: x * y,
+    '/': lambda x, y: x / y,
+             }
 
 
 def main():
@@ -56,20 +63,28 @@ def main():
         last_chat_name = last_update['message']['chat']['first_name']
 
         if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
-            my_bot.send_message(last_chat_id, 'Доброе утро, {}'.format(last_chat_name))
+            my_bot.send_message(last_chat_id, f'Доброе утро, {last_chat_name}')
             # today += 1
 
         elif last_chat_text.lower() in greetings and today == now.day and 12 <= hour < 17:
-            my_bot.send_message(last_chat_id, 'Добрый день, {}'.format(last_chat_name))
+            my_bot.send_message(last_chat_id, f'Добрый день, {last_chat_name}')
             # today += 1
 
         elif last_chat_text.lower() in greetings and today == now.day and 17 <= hour < 23:
-            my_bot.send_message(last_chat_id, 'Добрый вечер, {}'.format(last_chat_name))
+            my_bot.send_message(last_chat_id, f'Добрый вечер, {last_chat_name}')
             # today += 1
-
+        else:
+            try:
+                operand_1, operator, operand_2 = re.split(r'([+\-*/])', last_chat_text)
+                operand_1 = int(operand_1)
+                operand_2 = int(operand_2)
+                if operator in operators:
+                    result = operators[operator](operand_1, operand_2)
+                    my_bot.send_message(last_chat_id, f'{last_chat_text} = {result}')
+            except ValueError:
+                my_bot.send_message(last_chat_id, f'Я Вас не понимаю, {last_chat_name}')
         new_offset = last_update_id + 1
 
 
 if __name__ == '__main__':
     main()
-
